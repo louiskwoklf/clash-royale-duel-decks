94d83cf761b617ea81b502a1c182e4c8c3e966b5
louislfkwok <louiskwok0226@gmail.com>
2024-08-10 18:31:24 +0100

    first commit

---
+++ generate-dueldeck.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int UNIQUENESS = 32;
+
+const string CHOICES = "basic.txt";
+// const string CHOICES = "ultchamp.txt";
+// const string CHOICES = "tourney.txt";
+
+string cards[8];
+string decks[30][8];
+vector <pair <int, string> > duel_decks;
+
+int count_unique_cards(int index1, int index2, int index3, int index4) {
+    int uniqueness = 0;
+    set <string> unique_cards;
+    for (int i = 0; i < 8; i++) {
+        string card = decks[index1][i];
+        if (card[card.length() - 1] == '1') {
+            card.erase(card.length() - 4, 4);
+        }
+        unique_cards.insert(card);
+    }
+    for (int i = 0; i < 8; i++) {
+        string card = decks[index2][i];
+        if (card[card.length() - 1] == '1') {
+            card.erase(card.length() - 4, 4);
+        }
+        unique_cards.insert(card);
+    }
+    for (int i = 0; i < 8; i++) {
+        string card = decks[index3][i];
+        if (card[card.length() - 1] == '1') {
+            card.erase(card.length() - 4, 4);
+        }
+        unique_cards.insert(card);
+    }
+    for (int i = 0; i < 8; i++) {
+        string card = decks[index4][i];
+        if (card[card.length() - 1] == '1') {
+            card.erase(card.length() - 4, 4);
+        }
+        unique_cards.insert(card);
+    }
+    return unique_cards.size();
+}
+
+string stringify_deck(int deck_index, int deck_number) {
+    string return_string = "";
+    return_string += (char)(deck_number + '0');
+    return_string += ": ";
+    for (int i = 0; i < 8; i++) {
+        return_string += decks[deck_index][i] + (i == 7 ? "\n": ",");
+    }
+    return return_string;
+}
+
+int main() {
+    ifstream infile(CHOICES);
+    ofstream outfile("dueldecks.txt");
+    string s;
+    int deck_count = 0;
+    while(getline(infile, s)) {
+        string card = "";
+        int card_count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == ',') {
+                cards[card_count++] = card;
+                card = "";
+            }
+            else {
+                card += s[i];
+            }
+        }
+        while (card[card.length() - 1] == ' ') {
+            card.erase(card.length() - 1, 1);
+        }
+        cards[card_count++] = card;
+        for (int i = 0; i < 8; i++) {
+            decks[deck_count][i] = cards[i];
+        }
+        deck_count++;
+    }
+
+    for (int i = 0; i < deck_count; i++) {
+        for (int j = i + 1; j < deck_count; j++) {
+            for (int k = j + 1; k < deck_count; k++) {
+                for (int l = k + 1; l < deck_count; l++) {
+                    int uniqueness = count_unique_cards(i, j, k, l);
+
+                    string deck1 = stringify_deck(i, 1);
+                    string deck2 = stringify_deck(j, 2);
+                    string deck3 = stringify_deck(k, 3);
+                    string deck4 = stringify_deck(l, 4);
+        
+                    string final_string = deck1 + deck2 + deck3 + deck4;
+                    duel_decks.push_back(make_pair(uniqueness, final_string));
+                }
+            }
+        }
+    }
+
+    sort(duel_decks.begin(), duel_decks.end());
+    reverse(duel_decks.begin(), duel_decks.end());
+
+    set <string> duel_decks_final;
+
+    for (int i = 0; i < duel_decks.size(); i++) {
+        if (duel_decks[i].first == UNIQUENESS) {
+            duel_decks_final.insert(duel_decks[i].second);
+        }
+    }
+
+    cout << duel_decks_final.size() << endl;
+
+    for (auto i = duel_decks_final.begin(); i != duel_decks_final.end(); i++) {
+        outfile << *i << endl;
+    }
+
+    infile.close();
+    outfile.close();
+    return 0;
+}
---
+++ generate-html.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const string OUTPUT = "basic.html";
+// const string OUTPUT = "ultchamp.html";
+// const string OUTPUT = "tourney.html";
+
+string formatCardName(const string& cardName) {
+    string formattedName = cardName;
+    transform(formattedName.begin(), formattedName.end(), formattedName.begin(), ::tolower);
+    replace(formattedName.begin(), formattedName.end(), ' ', '-');
+    return formattedName;
+}
+
+string generateDeckHtml(const vector<string>& deck) {
+    ostringstream html;
+    html << "        <div class=\"deck\">\n";
+    for (const auto& card : deck) {
+        string formattedCardName = formatCardName(card);
+        if (!formattedCardName.empty()) {
+            html << "            <div class=\"card\">\n"
+                 << "                <img src=\"https://cdns3.royaleapi.com/cdn-cgi/image/w=150,h=180,format=auto/static/img/cards/v3-2d286f92/"
+                 << formattedCardName << ".png\" alt=\"" << card << "\" />\n"
+                 << "                <p>" << card << "</p>\n"
+                 << "            </div>\n";
+        } else {
+            html << "            <div class=\"card\"></div>\n";
+        }
+    }
+    html << "        </div>\n";
+    return html.str();
+}
+
+int main() {
+    ifstream infile("dueldecks.txt");
+    if (!infile.is_open()) {
+        cerr << "Error opening file for reading" << endl;
+        return 1;
+    }
+
+    ofstream htmlOutput(OUTPUT);
+    if (!htmlOutput.is_open()) {
+        cerr << "Error opening file for writing" << endl;
+        return 1;
+    }
+
+    string line;
+    int deckCount = 0;
+
+    htmlOutput << "<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n"
+               << "    <meta charset=\"UTF-8\">\n"
+               << "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+               << "    <title>Clash Royale Decks</title>\n"
+               << "    <link rel=\"stylesheet\" href=\"styles.css\">\n"
+               << "</head>\n<body>\n";
+
+    while (getline(infile, line)) {
+        if (line.empty()) continue;
+
+        istringstream iss(line);
+        string token;
+        vector<string> deck;
+
+        getline(iss, token, ':');
+
+        while (getline(iss, token, ',')) {
+            token.erase(0, token.find_first_not_of(' '));
+            deck.push_back(token);
+        }
+
+        if (deckCount % 4 == 0) {
+            if (deckCount > 0) {
+                htmlOutput << "    </div>\n";
+            }
+            htmlOutput << "    <div class=\"deck-container\">\n";
+        }
+
+        htmlOutput << generateDeckHtml(deck);
+
+        deckCount++;
+    }
+
+    if (deckCount > 0) {
+        htmlOutput << "    </div>\n";
+    }
+
+    htmlOutput << "</body>\n</html>";
+
+    infile.close();
+    htmlOutput.close();
+
+    return 0;
+}
